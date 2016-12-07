a27312b94a390cb795b61138fbe4705bf75d57b0
German Allemand <zweimal@gmail.com>
2016-12-07 18:15:57 -0300

    Updated declaration
    Added missing function hasPermission

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,8 @@
 //-------------------------
 declare module 'nativescript-permissions' {
 
+  export function hasPermission(permission: any): boolean;
+
   export function requestPermissions(permissions:Array<any>, explanation?:string):Promise<any>;
 
   export function requestPermission(permission:any, explanation?:string):Promise<any>;
