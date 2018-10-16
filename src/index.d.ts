73bb2933195f4439b6efdeccada1167e394f6d0f
Mathieu TUDISCO <dev@mathieutu.ovh>
2018-10-16 19:51:49 +0200

    Remove module in Typescript typing.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,11 @@
 //--------------------------
 // nativescript-permissions typings file.
 //-------------------------
-declare module 'nativescript-permissions' {
 
-  export function hasPermission(permission: any): boolean;
+export function hasPermission(permission: any): boolean;
 
-  export function requestPermissions(permissions:Array<any>, explanation?:string):Promise<any>;
+export function requestPermissions(permissions:Array<any>, explanation?:string):Promise<any>;
 
-  export function requestPermission(permission:any, explanation?:string):Promise<any>;
+export function requestPermission(permission:any, explanation?:string):Promise<any>;
 
-  export function hasPermission(permission: any): boolean;
-
-}
-
+export function hasPermission(permission: any): boolean;
